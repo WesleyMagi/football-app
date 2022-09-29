8814b73a06387cef833a18d92b091f296c616f9a
Wesley <wesley.magi@bao-solutions.com>
2022-09-30 01:48:14 +0200

    Add search bar and team cards.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import {
   ChakraProvider,
   Box,
@@ -9,14 +9,28 @@ import {
   Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+
+import SearchBar from './components/SearchBar';
+import TeamSource from './api/TeamSource';
+import TeamListCard from './components/TeamCardList';
 
 function App() {
+
+  const [state, setState] = useState({
+    results: []
+  });
+
+  const onSearch = async (text) => {
+    const results = await TeamSource.get()
+    setState(prevState => {
+      return {...prevState, results:results}
+    })
+  }
+
   return (
     <ChakraProvider theme={theme}>
       <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
+        {/* <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
             <Logo h="40vmin" pointerEvents="none" />
@@ -33,7 +47,10 @@ function App() {
               Learn Chakra
             </Link>
           </VStack>
-        </Grid>
+        </Grid> */}
+        <h2>Football App</h2>
+        <SearchBar onSearch={onSearch} />
+        <TeamListCard results={state.results}/>
       </Box>
     </ChakraProvider>
   );
