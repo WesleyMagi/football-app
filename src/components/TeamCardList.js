6310b11c06af1182b18185bd661ee2ca54b80373
Wesley <wesley.magi@bao-solutions.com>
2022-09-30 02:04:22 +0200

    Add card list with mapping functionality for card display.

---
+++ src/components/TeamCardList.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TeamCard from "./TeamCard";
+
+function TeamListCard({ results }) {
+    let data = [];
+    if (results.data) {
+        data = results.data.teams || [];
+    }
+    console.log(data)
+    return(
+        <div className="result">
+            {data.map((item) => (
+                <TeamCard key={item.id} team={item}/>
+            ))}
+            
+        </div>
+    )
+}
+
+export default TeamListCard;
